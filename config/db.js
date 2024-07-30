95d0145c6ebf141f2ba5b1213c2bb9f0732d8514
dvverma03 <dvverma9211@gmail.com>
2024-07-31 01:29:33 +0530

    update uri

--- config/db.js
+++ config/db.js
@@ -1,13 +1,20 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { DB_NAME } = require("../constant");
 
-const connectDB= async()=>{
-    try{
-        const connection = await mongoose.connect(process.env.MONGODB_URI)
-        console.log("mongoDB connected", connection.connection.host)
-    }catch(error){
-        console.log("Error", error.message)
-        process.exit()
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("Error:", error.message);
+        process.exit(1);
     }
-}
+};
 
-module.exports=connectDB
+module.exports = connectDB;
---
+++ constant.js
@@ -0,0 +1,2 @@
+const DB_NAME = "chatApp"
+module.exports ={DB_NAME}
