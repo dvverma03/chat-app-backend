b1602b7c69ace5440f92619169af2c1feff60cbf
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-07-31 04:09:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const server= app.listen(1234, console.log(`Server is running on ${PORT}`));
 const io= require('socket.io')(server,{
     pingTimeout:60000,
     cors:{
-        origin:"http://localhost:3000"
+        origin:["http://localhost:3000","https://chat-app-frontend-03.vercel.app"]
     }
 })
 
